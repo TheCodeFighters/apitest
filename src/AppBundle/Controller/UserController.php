56499d2b5e253586d5048855d818b8b0b73ea08b
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2017-12-20 16:42:47 +0100

    swagger annotations and restful conventions

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,10 +7,12 @@ use FOS\RestBundle\Controller\Annotations\RouteResource;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use AppBundle\Entity\Message;
 use Swagger\Annotations as SWG;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 
 
 /**
- * @RouteResource("User", pluralize=false)
+ * @RouteResource("Provider", pluralize=false)
  * Class MesssageController
  * @package AppBundle\Controller
  */
@@ -26,6 +28,13 @@ class UserController extends Controller
      *     )
      * )
      * @SWG\Parameter(
+     *     name="provider",
+     *     in="query",
+     *     required=true,
+     *     type="string",
+     *     description="The field used to select provider for the query"
+     * )
+     * @SWG\Parameter(
      *     name="username",
      *     in="path",
      *     required=true,
@@ -34,29 +43,26 @@ class UserController extends Controller
      * )
      * @SWG\Parameter(
      *     name="numberOfMessages",
-     *     in="path",
+     *     in="query",
      *     required=true,
      *     type="integer",
      *     description="The field used to select number of messages returned by the endpoint"
      * )
-     * @SWG\Parameter(
-     *     name="provider",
-     *     in="path",
-     *     required=true,
-     *     type="string",
-     *     description="The field used to select provider for the query"
-     * )
      * @SWG\Tag(name="message", description="describe a Message Object with id and text")
-     *
      * @param string $username
      * @param int $numberOfMessages
      * @param string $provider
+     * @param ParamFetcher $paramFetcher
      * @return AppBundle\Entity\Message[]
      */
-    public function getNumberofmessagesProviderAction( string $username,int $numberOfMessages,string $provider): array
+    public function getUserMessagesAction( string $provider,string $username,ParamFetcher $paramFetcher): array
     {
+        $dynamicQueryParam = new QueryParam();
+        $dynamicQueryParam->name = "numberOfMessages";
+        $dynamicQueryParam->requirements="\d+";
+        $paramFetcher->addParam($dynamicQueryParam);
         $service = $this->container->get('app.message_service');
-        $messages = $service->getUserMessages($username, $numberOfMessages);
+        $messages = $service->getUserMessages($username, $paramFetcher->get('numberOfMessages'));
         return $messages;
     }
 }
