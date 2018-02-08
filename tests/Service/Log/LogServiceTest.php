a53cf08351e63baee160a30514824ae953730fbf
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-02-08 19:17:27 +0100

    "testing"

--- tests/Service/Log/LogServiceTest.php
+++ tests/Service/Log/LogServiceTest.php
@@ -8,66 +8,31 @@
 
 namespace App\Tests\Service\Log;
 
-use App\Repository\Message\MessageRequestRepository;
 use PHPUnit\Framework\TestCase;
 use App\Entity\Message\MessageRequest;
 use App\Entity\Message\Message;
 use App\Repository\Message\MessageRequestRepository;
 use App\Command\Message\GetMessagesCommand;
 use App\Service\Log\LogService;
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 
 
 class LogServiceTest extends TestCase
 {
+    private $messageRequestRepository;
+    public function setUp()
+    {
+        $this->messageRequestRepository = $this->createMock(MessageRequestRepository::class);
+    }
+
     public function testPersistMessageRequest()
     {
-        $getMessagesCommand  = new GetMessagesCommand(1,'letsbonus');
-
-        $messageRequest = new MessageRequest($getMessagesCommand);
-        $message = new Message(0,"texto mockeado0",$messageRequest);
-        $messageRequest->addMessage($message);
-        $message = new Message(1,"texto mockeado1",$messageRequest);
-        $messageRequest->addMessage($message);
-
-
-        // Now, mock the repository so it returns the mock of the employee
-        $messageRequestRepository = $this->createMock(MessageRequestRepository::class);
-        // use getMock() on PHPUnit 5.3 or below
-        // $employeeRepository = $this->getMock(ObjectRepository::class);
-        $messageRequestRepository->expects($this->any())
+        $this->messageRequestRepository->expects($this->once())
             ->method('save')
-            ->willReturn($employee);
-
-        // Last, mock the EntityManager to return the mock of the repository
-        $objectManager = $this->createMock(ObjectManager::class);
-        // use getMock() on PHPUnit 5.3 or below
-        // $objectManager = $this->getMock(ObjectManager::class);
-        $objectManager->expects($this->any())
-            ->method('getRepository')
-            ->willReturn($messageRequestRepository);
-
-        $salaryCalculator = new SalaryCalculator($objectManager);
-        $this->assertEquals(2100, $salaryCalculator->calculateTotalSalary(1));
-
-
-
-
-
-        $logService = $this->getMockBuilder('App\Service\Log\LogService')
-            ->setConstructorArgs(array($messageRequest))
-            ->getMock();
-        $logService->persistMessageRequest($messageRequest);
-
-        $this->assertEquals(1, 1);
+            ->willReturn(null);
+        $messageRequest = $this->createMock(MessageRequest::class);
+        $logService = new LogService($this->messageRequestRepository);
+        $this->assertEquals(null, $logService->persistMessageRequest($messageRequest));
     }
 
-    private function castObject($className,$instance){
-        return unserialize(sprintf(
-            'O:%d:"%s"%s',
-            strlen($className),
-            $className,
-            strstr(strstr(serialize($instance), '"'), ':')
-        ));
-    }
 }
