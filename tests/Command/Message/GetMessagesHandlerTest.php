ac1cc99313331502e5afa7fa7062eef35520267f
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-02-13 20:21:08 +0100

    "playing"

--- tests/Command/Message/GetMessagesHandlerTest.php
+++ tests/Command/Message/GetMessagesHandlerTest.php
@@ -18,55 +18,103 @@ use JMS\Serializer\SerializerBuilder;
 
 class GetMessagesHandlerTest extends TestCase
 {
+
+    private $messageService;
+    private $messageInfo;
+    private $command;
+    private $cache;
+    private $cacheItem;
+    private $eventDispatcher;
+    private $serializer;
+
     public function setUp()
     {
-
-    }
-
-    public function testHandleWithCache()
-    {
-        $messageService = $this->getMockBuilder('App\Service\Message\MessageServiceInterface')
+        $this->messageService = $this->getMockBuilder('App\Service\Message\MessageServiceInterface')
             ->getMockForAbstractClass();
-        $messageInfo = array(
+        $this->messageInfo = array(
             array(
                 "full_text" => "mensaje de ejemplo",
                 "id" => 1
             )
         );
-        $messageService->expects($this->any())
+        $this->messageService->expects($this->any())
             ->method('getMessagesByUsernameAndNumberOfMessages')
-            ->will($this->returnValue($messageInfo));
+            ->will($this->returnValue($this->messageInfo));
 
-        $command = $this->createMock(GetMessagesCommand::class);
-        $command->expects($this->any())
+        $this->command = $this->createMock(GetMessagesCommand::class);
+        $this->command->expects($this->any())
             ->method('getNumberOfMessages');
-        $command->expects($this->any())
+        $this->command->expects($this->any())
             ->method('getUsername');
 
-        $cache = $this->createMock(AdapterInterface::class);
-
-
-        $cacheItem = $this->getMockBuilder(CacheItemInterface::class)
+        $this->cache = $this->createMock(AdapterInterface::class);
+        $this->cacheItem = $this->getMockBuilder(CacheItemInterface::class)
             ->getMockForAbstractClass();
-        $cacheItem->expects($this->once())
+        $this->cache->expects($this->any())
+            ->method('getItem')
+            ->will($this->returnValue($this->cacheItem));
+        $this->eventDispatcher= $this->createMock(EventDispatcher::class);
+        $this->serializer = SerializerBuilder::create()->build();
+    }
+
+    public function testHandleWithCache()
+    {
+
+        $this->cacheItem->expects($this->once())
             ->method('isHit')
             ->will($this->returnValue(false));
 
-        $cache->expects($this->any())
-            ->method('getItem')
-            ->will($this->returnValue($cacheItem));
-        $eventDispatcher= $this->createMock(EventDispatcher::class);
+        $getMessagesHandler = new GetMessagesHandler($this->cache,$this->messageService,$this->eventDispatcher);
+        $output =  $getMessagesHandler->handle($this->command);
+        $messageRequest = new MessageRequest($this->command);
+        $message = new Message($this->messageInfo[0]["id"],$this->messageInfo[0]["full_text"],$messageRequest);
 
-        $getMessagesHandler = new GetMessagesHandler($cache,$messageService,$eventDispatcher);
-        $output =  $getMessagesHandler->handle($command);
-
-        $messageRequest = new MessageRequest($command);
-
-        $message = new Message($messageInfo[0]["id"],$messageInfo[0]["full_text"],$messageRequest);
-
-
-        $serializer = SerializerBuilder::create()->build();
-        $this->assertEquals($serializer->serialize(array($message), 'json'),$serializer->serialize($output, 'json'));
+        $this->assertEquals($this->serializer->serialize(array($message), 'json'),$this->serializer->serialize($output, 'json'));
     }
+
+//    public function testHandleWithoutCache()
+//    {
+//        $messageService = $this->getMockBuilder('App\Service\Message\MessageServiceInterface')
+//            ->getMockForAbstractClass();
+//        $messageInfo = array(
+//            array(
+//                "full_text" => "mensaje de ejemplo",
+//                "id" => 1
+//            )
+//        );
+//
+//        $command = $this->createMock(GetMessagesCommand::class);
+//        $command->expects($this->any())
+//            ->method('getNumberOfMessages');
+//        $command->expects($this->any())
+//            ->method('getUsername');
+//
+//        $cache = $this->createMock(AdapterInterface::class);
+//        $cacheItem = $this->getMockBuilder(CacheItemInterface::class)
+//            ->getMockForAbstractClass();
+//        $cacheItem->expects($this->once())
+//            ->method('isHit')
+//            ->will($this->returnValue(true));
+//
+//        $cache->expects($this->any())
+//            ->method('getItem')
+//            ->will($this->returnValue($cacheItem));
+//
+//        $cacheItem->expects($this->once())
+//            ->method('get')
+//            ->will($this->returnValue($messageInfo));
+//
+//
+//
+//        $eventDispatcher= $this->createMock(EventDispatcher::class);
+//
+//        $getMessagesHandler = new GetMessagesHandler($cache,$messageService,$eventDispatcher);
+//        $output =  $getMessagesHandler->handle($command);
+//        $messageRequest = new MessageRequest($command);
+//        $message = new Message($messageInfo[0]["id"],$messageInfo[0]["full_text"],$messageRequest);
+//
+//        $serializer = SerializerBuilder::create()->build();
+//        $this->assertEquals($serializer->serialize(array($message), 'json'),$serializer->serialize($output, 'json'));
+//    }
 
 }
