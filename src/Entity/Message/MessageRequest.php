529c7ef7dcbe28b452dbdc79f8349e1cd7f1a340
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-02-13 20:00:37 +0100

    "playing"

--- src/Entity/Message/MessageRequest.php
+++ src/Entity/Message/MessageRequest.php
@@ -4,7 +4,6 @@ namespace App\Entity\Message;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Command\Message\GetMessagesCommand;
-use Doctrine\ORM\PersistentCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
